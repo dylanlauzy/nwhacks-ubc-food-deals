cf61231bae869b4441a76cee4f50ff33710a63fb
markishou <myhou0720@gmail.com>
2022-11-06 18:13:03 -0800

    Fixed card image/populated app with data

--- src/components/OptionList.js
+++ src/components/OptionList.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import Option from "./Option";
 import fill from "./images/fill.png";
+import subway from "./images/subway.png"
+import nori from "./images/nori.png"
+import bodyclub from "./images/bodyclub.jpeg"
+import unclefatih from "./images/unclefatih.jpeg"
+import mcdonalds from "./images/mcdonalds.jpeg"
+import chatime1 from "./images/chatime1.jpeg"
+import freshii from "./images/freshii.png"
 import "./Option.css";
 import { type } from "@testing-library/user-event/dist/type";
 
@@ -15,37 +22,61 @@ const OptionList = () => {
   //TODO: use typeEnum in state to trigger rerender when filtering
   const [options, setOptions] = useState([
     {
-      picPath: fill,
+      picPath: unclefatih,
       name: "Uncle Fatih's",
-      deal: "2 for 1 medium pizza",
+      deal: "Buy 1 medium pizza get 1 for free.",
       expDate: "Exp. Nov 22, 2022",
       foodType: typeEnum.quickMeal,
       id: 1,
     },
     {
-      picPath: fill,
+      picPath: nori,
       name: "Nori",
-      deal: "20% Niku Tama",
+      deal: "20% off Niku Tama with Student Card",
       expDate: "Exp. Dec 1, 2022",
       foodType: typeEnum.quickMeal,
       id: 2,
     },
     {
-      picPath: fill,
+      picPath: subway,
       name: "Subway",
-      deal: "10% off any footlong",
+      deal: "10% off any footlong with drink purchase",
       expDate: "Exp. January 1, 2022",
       foodType: typeEnum.healthy,
       id: 3,
     },
     {
-      picPath: fill,
+      picPath: bodyclub,
       name: "Body Energy Club",
-      deal: "Buy 1 Smoothie get 1 free",
+      deal: "Buy 1 Smoothie get 1 for 50% off",
       expDate: "Exp. Nov 30, 2022",
       foodType: typeEnum.drink,
       id: 4,
     },
+    {
+      picPath: mcdonalds,
+      name: "McDonald's",
+      deal: "App coupons available (includes 4.99 Bic Macs)",
+      expDate: "Exp. December 1, 2022",
+      foodType: typeEnum.quickMeal,
+      id: 5       
+    },
+    {
+      picPath: chatime1,
+      name: "Chatime",
+      deal: "Buy 2 drinks get the 3rd one free",
+      expDate: "Exp. December 1, 2022",
+      foodType: typeEnum.drink,
+      id: 6  
+    }, 
+    {
+      picPath: freshii,
+      name: "Freshii",
+      deal: "Buy one get one Kung Fu Bowl",
+      expDate: "Exp. January 1, 2022",
+      foodType: typeEnum.healthy,
+      id: 7  
+    }
   ]);
 
   const [filter, setFilter] = useState("");
